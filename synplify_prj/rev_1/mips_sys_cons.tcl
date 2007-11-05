ff6c1a9b1344c2eaa1b86a4dea1cd6aeb08e6060
mcupro <mcupro@a6c65ded-335b-465d-a0f9-4d6f221586b5>
2007-11-05 21:38:48 +0000

    no message

--- synplify_prj/rev_1/mips_sys_cons.tcl
+++
@@ -1,6 +0,0 @@
-source "C:/Program Files/Synplicity/fpga_81/lib/altera/quartus_cons.tcl"
-syn_create_and_open_prj mips_sys
-source $::quartus(binpath)/prj_asd_import.tcl
-syn_create_and_open_csf mips_sys
-syn_handle_cons mips_sys
-syn_compile_quartus
