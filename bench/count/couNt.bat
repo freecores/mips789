1cfb3bc32ed75c19116a49413bcddc8db9ffabb4
mcupro <mcupro@a6c65ded-335b-465d-a0f9-4d6f221586b5>
2007-11-05 22:59:05 +0000

    no message

---
+++ bench/count/couNt.bat
@@ -0,0 +1,17 @@
+path;
+path=..\..\gccmips_elf
+del *.axf
+del *.txt
+as -o boot.o ..\plasmaboot.asm
+gcc  -O2 -O  -Wall -c -s ..\..\clib\dvc_lib.c
+gcc  -O2 -O  -Wall -c -s count.c
+
+ld.exe -Ttext 0 -eentry -Map test.map -s -N -o test.axf boot.o dvc_lib.o count.o
+
+objdump.exe --disassemble test.axf > list.txt
+
+convert_sp 
+gensim 
+genmif 
+#ser_dld 9600 COM1 N
+copy *.mif ..\..\quartus2\*.mif
